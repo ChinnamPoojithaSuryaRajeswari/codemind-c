f34d1f83a3ba2df34017dd25d8daa3ebe6d6f04b
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2022-12-18 20:32:30 +0530

    #include<stdio.h> int main() {     int n,m,i,q,w;     scanf("%d%d",&n,&m);     q=n+1;     w=m-1;     for(i=q;i<=w;i++)     {         printf("%d %d %d ",i,i*i,i*i*i);     } }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,q,w;
+    scanf("%d%d",&n,&m);
+    q=n+1;
+    w=m-1;
+    for(i=q;i<=w;i++)
+    {
+        printf("%d %d %d
+",i,i*i,i*i*i);
+    }
+}
