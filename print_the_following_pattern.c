0e388d07aa654a52498f33aec2db5638e7dee1aa
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-01-24 07:25:22 +0530

    #include<stdio.h> int main()     {         int n,i,j;         scanf("%d",&n);         for(i=n;i>=1;i--)         {             for(j=1;j<=i;j++)             {                 printf("%d",j);             }             printf(" ");         }     }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
-{
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        int n,i,j;
+        scanf("%d",&n);
+        for(i=n;i>=1;i--)
         {
-            printf("%d",j);
-        }
-        printf("
+            for(j=1;j<=i;j++)
+            {
+                printf("%d",j);
+            }
+            printf("
 ");
+        }
     }
-}
