fc2191e14934ed80989ac798781de8981900069f
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-01-24 21:04:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,m;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         }         int b,c;         scanf("%d%d",&b,&c);         m=a[0];         for(i=1;i<n;i++)         {             if(m<a[i]&&!(a[i]>=b&&a[i]<=c))             {                 m=a[i];                 } }                 if(m==a[0])                 printf("-1");                 else printf("%d",m);}

--- maximum_of_numbers_not_between_A_and_B.c
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -4,18 +4,19 @@ int main()
     int n;
     scanf("%d",&n);
     int a[n],i,m;
-    for(i=0;i<n;i++) 
+    for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]); 
-            }
-            int b,c;
-            scanf("%d%d",&b,&c);
-            m=a[0];
-            for(i=1;i<n;i++)
+        scanf("%d",&a[i]);
+        }
+        int b,c; 
+        scanf("%d%d",&b,&c); 
+        m=a[0];
+        for(i=1;i<n;i++)
+        {
+            if(m<a[i]&&!(a[i]>=b&&a[i]<=c)) 
             {
-                if(m<a[i]&&!(a[i]>=b&&a[i]<=c)) 
-                { m=a[i]; } 
-                            } 
-                            if(m==a[0])
-                            printf("-1");
-                            else printf("%d",m);}
+                m=a[i];
+                } }
+                if(m==a[0]) 
+                printf("-1");
+                else printf("%d",m);}
