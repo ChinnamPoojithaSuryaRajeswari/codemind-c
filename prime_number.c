aa9c1ab2ffebbb90953ec2ccf11d5851b5128277
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-05-17 14:41:17 +0530

    #include<stdio.h> int main() {     int n,c=0,i;     scanf("%d",&n);     for(i=2;i<=n;i++)     {         if(n%i==0)         {             c=c+1;         }     }     if(c==1)     {         printf("prime");     }     else     {         printf("not a prime");         } }

--- prime_number.c
+++ prime_number.c
@@ -1,21 +1,21 @@
-c#include<stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,i,c=0;
+    int n,c=0,i;
     scanf("%d",&n);
     for(i=2;i<=n;i++)
     {
         if(n%i==0)
         {
-            c++;
+            c=c+1;
         }
     }
-    if(c++==1)
+    if(c==1)
     {
         printf("prime");
     }
     else
     {
         printf("not a prime");
-    }
+        }
 }
