c17c4734b28c4f740734d744a36c8ffb8704d249
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-05-22 14:23:59 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0 && i!=0 && i!=n-1)         {             if(a[i-1]%2!=0 && a[i+1]%2!=0)             {                 c=c+1;             }         }     }     printf("%d",c); }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0 && i!=0 && i!=n-1)
+        {
+            if(a[i-1]%2!=0 && a[i+1]%2!=0)
+            {
+                c=c+1;
+            }
+        }
+    }
+    printf("%d",c);
+}
