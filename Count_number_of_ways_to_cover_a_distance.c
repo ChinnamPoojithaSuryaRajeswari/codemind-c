a3258ac687c96fd06f149eb96838617e8da49195
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-06-01 10:52:00 +0530

    #include<stdio.h> int count(int n) {     if(n<0)     {         return 0;     }     else if(n == 0)     {         return 1;     } return count(n-1)+count(n-2)+count(n-3); } int main() {     int n;     scanf("%d",&n);     printf("%d",count(n)); }

---
+++ Count_number_of_ways_to_cover_a_distance.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int count(int n)
+{
+    if(n<0)
+    {
+        return 0;
+    }
+    else if(n == 0)
+    {
+        return 1;
+    }
+return count(n-1)+count(n-2)+count(n-3);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",count(n));
+}
