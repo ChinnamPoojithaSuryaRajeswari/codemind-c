8195be4328f7a6979e89869f3721fea3831fa81a
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-06-02 13:13:25 +0530

    #include<stdio.h> int main() {     int n,i,j,c;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++){             if(a[i]==a[j])             {                 c=c+1;             }         }         if(c==1)         {             printf("%d ",a[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++){
+            if(a[i]==a[j])
+            {
+                c=c+1;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
