0c5d7d4c704ae21ca99100007c888bbe73e5fd84
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-06-02 12:43:44 +0530

    #include<stdio.h> int main() {     int n,i,s=0,c,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         c=0;         if(n%i==0)         {             for(j=2;j<=i;j++)             {                 if(i%j==0)                 {                     c=c+1;                 }             }             if(c!=1)             {                 s=s+1;             }         }     }     printf("%d",s); }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c,s=0;
+    int n,i,s=0,c,j;
     scanf("%d",&n);
-    for(i=2;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
+        c=0;
         if(n%i==0)
         {
-            c=0;
-            for(j=1;j<=i;j++)
+            for(j=2;j<=i;j++)
             {
                 if(i%j==0)
                 {
                     c=c+1;
                 }
             }
-            if(c>2)
+            if(c!=1)
             {
                 s=s+1;
             }
         }
     }
-    printf("%d",s+1);
+    printf("%d",s);
 }
