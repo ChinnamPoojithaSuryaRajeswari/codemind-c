008489099ad5bb53b8d1dbfa9b5bbb38dcc632f4
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-06-03 16:49:31 +0530

    # include <stdio.h> # include <string.h> int main() {     int n,x=0;     scanf("%d",&n);     while (n != 1)     {         if (n % 5 == 0)         {             n /= 5;             }             else if (n % 3 == 0)             { n /= 3;             }             else if (n % 2 == 0)             {                 n /= 2;                 }                 else                 {                     printf("Not Ugly Number");                     x = 1;                     break;                     }                     }                     if (x==0)                     {                         printf("Ugly Number"); }}

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,33 +1,29 @@
 # include <stdio.h>
 # include <string.h>
-
 int main()
 {
-int n,x=0;
-  scanf("%d",&n);
-       while (n != 1) 
-       {  
-            if (n % 5 == 0) 
-            {  
-                n /= 5;  
-            } 
-                    else if (n % 3 == 0) 
-            {  
-                n /= 3;  
-            } 
+    int n,x=0;
+    scanf("%d",&n);
+    while (n != 1)
+    {
+        if (n % 5 == 0) 
+        {
+            n /= 5;
+            }
+            else if (n % 3 == 0) 
+            { n /= 3; 
+            }
             else if (n % 2 == 0) 
-            {  
-                n /= 2;  
-            } 
-            else 
-            {  
-                printf("Not Ugly Number"); 
-                x = 1;  
-                break;  
-            }  
-        } 
-		        if (x==0)
-		        { 
-                printf("Ugly Number");
+            {
+                n /= 2;
                 }
-}
+                else 
+                {
+                    printf("Not Ugly Number");
+                    x = 1;
+                    break;
+                    }
+                    }
+                    if (x==0) 
+                    {
+                        printf("Ugly Number"); }}
