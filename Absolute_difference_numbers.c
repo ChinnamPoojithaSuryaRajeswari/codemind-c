1b21aa6424ec8514cc72c30ca5fa4288556c0071
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2023-06-03 16:39:06 +0530

    #include<stdio.h> int main() {     int n,m,s=1,q,w,e=1,r,t,y,u,i,p=0,a,d=0;     scanf("%d%d",&n,&m);     q=n;     w=n;     while(m)     {         s=s*10; m=m-1;     }     t=s;     while(q)     {         e=e*10;         q=q/10;     }     y=e/t;     u=w/y;     while(s>=10)     {         i=w%10;         p=p*10+i;         w=w/10;         s=s/10;     }     while(p)     {         a=p%10;         d=d*10+a;         p=p/10;     }     if(d>u)     {         printf("%d",d-u);             }             else             { printf("%d",u-d); }
    }

--- Absolute_difference_numbers.c
+++ Absolute_difference_numbers.c
@@ -3,12 +3,11 @@ int main()
 {
     int n,m,s=1,q,w,e=1,r,t,y,u,i,p=0,a,d=0;
     scanf("%d%d",&n,&m);
-    q=n;
+    q=n; 
     w=n;
     while(m)
-    {
-        s=s*10;
-        m=m-1;
+    { 
+        s=s*10; m=m-1; 
     }
     t=s;
     while(q)
@@ -17,26 +16,25 @@ int main()
         q=q/10;
     }
     y=e/t;
-    u=w/y;
+    u=w/y; 
     while(s>=10)
     {
-        i=w%10;
+        i=w%10; 
         p=p*10+i;
         w=w/10;
-        s=s/10;
-    }
+        s=s/10; 
+    } 
     while(p)
     {
         a=p%10;
-        d=d*10+a;
+        d=d*10+a; 
         p=p/10;
     }
-    if(d>u)
+    if(d>u) 
     {
-        printf("%d",d-u);
-    }
-    else
-    {
-        printf("%d",u-d);
-    }
+        printf("%d",d-u); 
+            }
+            else
+            { printf("%d",u-d); }
+    
 }
